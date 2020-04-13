92bfae913573a283b32062be3da7cad8b3ac674f
Madhav Varshney <madhavarshney@gmail.com>
2020-04-13 01:43:26 -0700

    Use webpack, reorganize code, and improve homepage
    Many days of work ... in one commit

---
+++ src/home/home.js
@@ -0,0 +1,240 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
+window.toggleHours = function toggleHours(elId) {
+  const el = document.getElementById(elId);
+  if (el) {
+    el.classList.toggle('hours-visible');
+  }
+};
+
+function createCard({ name = '', description = '', zoomLink, hoursHtml, img }) {
+  const el = document.createElement('div');
+  el.id = name;
+  el.className = 'card';
+  el.innerHTML = `
+    ${img ? `<div class="avatar"><img src=${img}></div>` : ''}
+    <div class="title">${name}</div>
+    <div class='room-info'>
+      <strong>Room: </strong>
+      <a target='_blank' href='${zoomLink}'>
+        <button class='primary-action'>Join Zoom</button>
+      </a>
+      <button onclick="toggleHours('${name}')">Hours</button>
+    </div>
+    <div class="description">${description}</div>
+    <div class='hours-info'>
+      ${hoursHtml}
+    </div>
+  `;
+  return el;
+}
+
+function generateHours(data) {
+  const row = (a = '', b = '', c = '') => `<span>${a}</span> <span>${b}</span> <span>${c}</span>`;
+
+  return `
+    <div class='hours'>
+      <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+      ${data.map((d) => (
+        row(...d)
+      )).join('\n')}
+    </div>
+  `;
+}
+
+const data = {
+  tutoring: [
+    {
+      name: 'STEM Center',
+      description: 'Get help with Accounting, Biology, Chemistry, Computer Science, Economics, Math, Networking, Physics, The Garden, The Village, and more!',
+      img: 'assets/home/stemcenter.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/378611456',
+      hours: {
+        timings: {
+          1: {
+            from: [9, 0],
+            to: [21, 0],
+          },
+          2: {
+            from: [21, 0],
+            to: [23, 59],
+            details: 'CS Only',
+          },
+          3: {
+            from: [9, 0],
+            to: [15, 0],
+          },
+          4: {
+            from: [11, 0],
+            to: [17, 0],
+          },
+          5: {
+            from: [12, 0],
+            to: [23, 59],
+            details: 'CS Only',
+          },
+        },
+        days: {
+          mon: [1, 2],
+          tue: [1, 2],
+          wed: [1, 2],
+          thu: [1, 2],
+          fri: 3,
+          sat: 4,
+          sun: 5,
+        },
+      },
+      hoursHtml: generateHours([
+        ['Mon – Thu:', '9 am', '9 pm'],
+        ['', '9 pm', '12 midnight<br>(CS Only)'],
+        ['Fri:', '9 am', '3 pm'],
+        ['Sat:', '11 am', '5 pm'],
+        ['Sun:', '12 noon', '12 midnight<br>(CS Only)'],
+      ]),
+    },
+    {
+      name: 'Teaching & Learning Center',
+      description: 'Get support for reading and writing in all subjects.',
+      img: 'assets/home/tlc.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/378611456',
+      hours: {},
+      hoursHtml: generateHours([
+        ['Mon, Wed, Fri:', 'Noon', '6 pm'],
+        ['Tue, Thu:', '9 am', 'Noon'],
+        ['', '3 pm', '9 pm'],
+      ]),
+      // hoursHtml: `
+      //   <div class='hours'>
+      //     <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+      //     <span>Mon, Wed, Fri:</span> <span>Noon</span> <span>6 pm</span>
+      //     <span>Tue, Thu:</span> <span>9 am</span> <span>Noon</span>
+      //     <span></span> <span>3 pm</span> <span>9 pm</span>
+      //   </div>
+      // `,
+    },
+    {
+      name: 'Pass the Torch',
+      description: 'Peer-to-peer tutoring for core courses in English, ESL, and Math.',
+      img: 'assets/home/passtorch.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/378611456',
+      hours: {},
+      hoursHtml: `
+        <div class='hours'>
+          <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+          <span>Mon – Thu:</span> <span>8:30 am</span> <span>5 pm</span>
+          <span>Fri:</span> <span>8 am</span> <span>Noon</span>
+        </div>
+      `,
+    },
+  ],
+  techHelp: [
+    {
+      name: 'Virtual Student Technology Hub',
+      description: 'Learn to use Zoom and Canvas, request help  for needed resources, such as laptops and wifi, and get tips to successfully move online!',
+      img: 'assets/home/techhub.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/950296669',
+      hours: {},
+      hoursHtml: `
+        <div class='hours'>
+          <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+          <span>Mon – Thu:</span> <span>8 am</span> <span>11 am</span>
+          <span></span> <span>2 pm</span> <span>5 pm </span>
+          <span>Fri:</span> <span>9 am</span> <span>Noon</span>
+        </div>
+      `,
+    },
+    {
+      name: 'Technology Hub 2',
+      zoomLink: 'https://cccconfer.zoom.us/j/950296669',
+      description: '',
+      hours: {},
+      hoursHtml: `
+        <div class='hours'>
+          <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+          <span>Mon – Thu:</span> <span>8 am</span> <span>11 am</span>
+          <span></span> <span>2 pm</span> <span>5 pm </span>
+          <span>Fri:</span> <span>9 am</span> <span>Noon</span>
+        </div>
+      `,
+    },
+    {
+      name: 'Technology Hub 3',
+      zoomLink: 'https://cccconfer.zoom.us/j/950296669',
+      description: '',
+      hours: {},
+      hoursHtml: `
+        <div class='hours'>
+          <div class='hours-title'>Hours:</div><span>From</span><span>To</span>
+          <span>Mon – Thu:</span> <span>8 am</span> <span>11 am</span>
+          <span></span> <span>2 pm</span> <span>5 pm </span>
+          <span>Fri:</span> <span>9 am</span> <span>Noon</span>
+        </div>
+      `,
+    },
+  ],
+  stayConnected: [
+    {
+      name: 'Tea with President Thuy',
+      description: 'Meet with Foothill President Thuy Nguyen! Get your questions answered and hear updates on the virtual campus and other news.',
+      img: 'assets/home/teawithpresident.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/431174212',
+      hours: {},
+      hoursHtml: `
+        <div class=''>
+          <div class='hours-title'>Hours:</div>
+          <b>Wednedays & Thursdays @ Noon</b>
+        </div>
+      `,
+    },
+    {
+      name: 'Chancellor\'s Open Office Hour with Judy Miner',
+      description: 'Join a weekly Zoom meeting for students, faculty and staff to share thoughts and questions with our district chancellor.',
+      img: 'assets/home/openoffice.jpg',
+      zoomLink: 'https://cccconfer.zoom.us/j/908990346',
+      hours: {},
+      hoursHtml: `
+        <div class=''>
+          <div class='hours-title'>Hours:</div>
+          <b>Wednesdays, March 25 – April 22 @ 11 AM</b>
+        </div>
+      `,
+    },
+    {
+    //   name: 'Chancellor\'s Open Office Hour with Judy Miner',
+    //   zoomLink: 'https://cccconfer.zoom.us/j/431174212',
+    //   description: 'Meet with Foothill President Thuy Nguyen! Get your questions answered and hear updates on the virtual campus and other news.',
+    //   hours: {},
+    //   hoursHtml: `
+    //     <div class=''>
+    //       <div class='hours-title'>Hours:</div>
+    //       <b>Wednesdays, March 25 – April 22 @ 11 AM</b>
+    //     </div>
+    //   `,
+    },
+  ],
+};
+
+let initStarted = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (initStarted) { return; }
+  initStarted = true;
+
+  document
+    .querySelector("#tutoring .row")
+    .append(...data.tutoring.map((c) => createCard(c)));
+  document
+    .querySelector("#tech-help .row")
+    .append(...data.techHelp.map((c) => createCard(c)));
+  document
+    .querySelector("#stay-connected .row")
+    .append(...data.stayConnected.map((c) => createCard(c)));
+});
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const Stylish = require('webpack-stylish');
+const WebpackBar = require('webpackbar');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+const stats = {
+  all: false,
+  colors: true,
+  errors: true,
+  warnings: true,
+  logging: 'warn',
+};
+const templateParams = {
+  title: 'Virtual Campus',
+  // NOTE: the following are custom parameters / this usage may break in the future
+  description: `An easy-to-use information center for Foothill's virtual campus.`,
+  websiteUrl: prod
+    ? 'https://madhavarshney.github.io/virtual-campus'
+    : 'http://localhost:8080',
+};
+
+module.exports = {
+  mode,
+  entry: {
+    home: ['./src/home/home.css', './src/home/home.js'],
+    clubs: ['./src/clubs/clubs.css', './src/clubs/clubs.js'],
+  },
+  devtool: prod ? false : 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: prod ? '[name].[contenthash:8].js' : '[name].js',
+  },
+  resolve: {},
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: './src/ejs-loader',
+        options: {
+          beautify: false,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1, sourceMap: !prod } },
+          { loader: 'postcss-loader', options: { sourceMap: !prod } },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  stats,
+  devServer: {
+    stats,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WebpackBar(),
+    new Stylish(),
+    new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }]),
+    new HtmlWebpackPlugin({
+      chunks: ['home'],
+      template: 'src/home/home.html',
+      filename: 'index.html',
+      // ...templateParams,
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['clubs'],
+      template: 'src/clubs/clubs.html',
+      filename: 'clubs.html',
+      // ...templateParams,
+    }),
+    new MiniCssExtractPlugin({
+      filename: prod ? '[name].[contenthash:8].css' : '[name].css',
+    }),
+    prod && new OptimizeCssAssetsPlugin(),
+    prod &&
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+      }),
+  ].filter(Boolean),
+};
